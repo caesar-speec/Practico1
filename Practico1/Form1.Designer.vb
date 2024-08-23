2db6d01197011be8e7dc3d0084c1842b3cfeac03
Pietro <pietrocalathaki@gmail.com>
2024-08-23 20:29:07 -0300

    Agregar archivos de proyecto.

---
+++ Practico1/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        BGuardar = New Button()
+        LApellido = New Label()
+        TextBox1 = New TextBox()
+        LNombre = New Label()
+        BEliminar = New Button()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' BGuardar
+        ' 
+        BGuardar.Location = New Point(64, 156)
+        BGuardar.Name = "BGuardar"
+        BGuardar.Size = New Size(75, 23)
+        BGuardar.TabIndex = 0
+        BGuardar.Text = "Guardar"
+        BGuardar.UseVisualStyleBackColor = True
+        ' 
+        ' LApellido
+        ' 
+        LApellido.AutoSize = True
+        LApellido.Location = New Point(12, 85)
+        LApellido.Name = "LApellido"
+        LApellido.Size = New Size(51, 15)
+        LApellido.TabIndex = 1
+        LApellido.Text = "Apellido"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(75, 28)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' LNombre
+        ' 
+        LNombre.AutoSize = True
+        LNombre.Location = New Point(12, 31)
+        LNombre.Name = "LNombre"
+        LNombre.Size = New Size(51, 15)
+        LNombre.TabIndex = 3
+        LNombre.Text = "Nombre"
+        ' 
+        ' BEliminar
+        ' 
+        BEliminar.Location = New Point(243, 156)
+        BEliminar.Name = "BEliminar"
+        BEliminar.Size = New Size(75, 23)
+        BEliminar.TabIndex = 4
+        BEliminar.Text = "Eliminar"
+        BEliminar.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(75, 82)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 5
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(203, 12)
+        TextBox3.Multiline = True
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(241, 104)
+        TextBox3.TabIndex = 6
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(315, 215)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 7
+        Button1.Text = "Fin"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(484, 261)
+        Controls.Add(Button1)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(BEliminar)
+        Controls.Add(LNombre)
+        Controls.Add(TextBox1)
+        Controls.Add(LApellido)
+        Controls.Add(BGuardar)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents BGuardar As Button
+    Friend WithEvents LApellido As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents LNombre As Label
+    Friend WithEvents BEliminar As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ Practico1/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub BGuardar_Click(sender As Object, e As EventArgs) Handles BGuardar.Click
+        Dim apellido As String = TextBox1.Text ' Assuming TextBox1 is for Apellido
+        Dim nombre As String = TextBox2.Text ' Assuming TextBox2 is for Nombre
+
+        ' Display the concatenated string in the third TextBox
+        TextBox3.Text = apellido & " " & nombre
+    End Sub
+
+    Private Sub BEliminar_Click(sender As Object, e As EventArgs) Handles BEliminar.Click
+        TextBox3.Clear()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        End
+    End Sub
+End Class
